b4eefe94662c678d384c41fc8d565547a8facfcf
Ayberk Gerey <ayberkgerey@gmail.com>
2021-11-26 23:46:26 +0300

    I think thats an odd.

--- src/containers/DriversContainer/index.js
+++ src/containers/DriversContainer/index.js
@@ -1,8 +1,29 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import {View, Text, StyleSheet} from 'react-native';
 import PrimeTopBar from '../../components/PrimeTopBar';
 
 export default function DriversContainer() {
+  const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  const fetchData = async () => {
+    fetch('https://ergast.com/api/f1/2021/drivers.json')
+      .then(response => response.json())
+      .then(responseJson => {
+        setLoading(false);
+        setData(responseJson);
+      })
+      .catch(error => {
+        console.error(error);
+      });
+  };
+
+  useEffect(() => {
+    fetchData();
+    console.log('Data : ' + JSON.stringify(data));
+    console.log('Loading : ' + loading);
+  }, []);
+
   return (
     <View style={styles.container}>
       <PrimeTopBar header="Drivers" />
